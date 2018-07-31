ae1eac334b0674a8df12a7fe0239968549f4f46a
Kondapa <41892141+Kondapa@users.noreply.github.com>
2018-07-31 10:51:06 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class vowel
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char str
+		if(str == 'a'||str == 'e'||str == 'i'||str == 'o'||str == 'u')
+		{
+			System.out.println(str + " is vowel");		
+		}
+		else
+		{
+			System.out.println(str +"is constant");
+		}
+	}
+}
