92ee276623c3f853be5e3ba2abdfcd29fe0bc156
Kondapa <41892141+Kondapa@users.noreply.github.com>
2018-08-01 05:52:19 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class positive
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n;
+		n = sc.nextInt();
+		if(n == 0)
+		{
+			System.out.println(n +" is zero");
+		}
+		else if (n > 0)
+		{
+			System.out.println(n +" is positive");
+		}
+		else
+		{
+			System.out.println(n +" is negativw");
+		}
+	}
+}
